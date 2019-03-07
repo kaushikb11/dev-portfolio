c3aa3b3c5350e8c9a50a9243c5ff5b738df76597
Kaushik Bokka <kaushikbokka@gmail.com>
2019-03-07 08:22:42 +0530

    .env

--- twitterAnalysis.js
+++ twitterAnalysis.js
@@ -7,7 +7,10 @@ const twitterAnalysis = function() {
     dotenv.config();
 
     const twitterApi = new twitter({
-
+        consumer_key: process.env.consumer_key,
+        consumer_secret: process.env.consumer_secret,
+        access_token_key: process.env.access_token_key,
+        access_token_secret: process.env.access_token_secret
     });
 
     const totalScore = [];
